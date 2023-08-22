bd42f896af3a4912f500df16eafd044eaec23db5
mabhishek7081 <mabhishek7081@gmail.com>
2023-08-23 02:21:04 +0530

    updated

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,46 +1,82 @@
-const { Telegraf } = require("telegraf")
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const { Telegraf, Markup } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Common function to handle topic selection
-function handleTopicSelection(ctx, topic) {
-  console.log(`User selected ${topic}`);
-  try {
-    ctx.reply(`Great choice! You selected ${topic}. Thanks!`);
-  } catch (e) {
-    console.error(`Error in ${topic} handler:`, e);
-  }
-}
+// Track selected options for each user
+const selectedOptions = {};
 
 bot.start(ctx => {
-  console.log("Received /start command");
-  try {
-    const welcomeMessage = "Welcome to Leetcode bot! Please select a topic:";
-    const options = {
-      reply_markup: {
-        keyboard: [["Array", "Linked List"]],
-        resize_keyboard: true,
-        one_time_keyboard: true
-      }
-    };
-    return ctx.reply(welcomeMessage, options);
-  } catch (e) {
-    console.error("error in start action:", e);
-    return ctx.reply("Error occurred");
-  }
-})
+    console.log("Received /start command");
+    try {
+        const welcomeMessage = "Welcome to Leetcode bot! Please select topics:";
+        const options = ['Array', 'String', 'Linked List', 'Doubly-Linked List',
+            'Dynamic Programming',
+            'Stack',
+            'Queue',
+            'Tree',
+            'Graph',
+            'Greedy',
+            'Hash Function',
+            'Hash Table',
+            'Heap (Priority Queue)',
+            'Backtracking',
+            'Binary Search',
+            'Binary Search Tree',
+            'Binary Tree',
+            'Breadth-First Search',
+            'Bucket Sort',
+            'Combinatorics',
+            'Concurrency',
+            'Counting',
+            'Counting Sort',
+            'Data Stream',
+            'Database',
+            'Depth-First Search',
+        ]
 
-bot.hears(/.*/, ctx => {
-  const selectedOption = ctx.message.text;
-  handleTopicSelection(ctx, selectedOption);
+        const keyboard = Markup.inlineKeyboard(
+            options.map(option => Markup.callbackButton(option, `select:${option}`))
+        );
+
+        return ctx.reply(welcomeMessage, keyboard.extra());
+    } catch (e) {
+        console.error("error in start action:", e);
+        return ctx.reply("Error occurred");
+    }
 });
 
-// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+bot.action(/select:(.*)/, ctx => {
+    const userId = ctx.from.id;
+    const selectedOption = ctx.match[1];
+
+    if (!selectedOptions[userId]) {
+        selectedOptions[userId] = [];
+    }
+
+    if (selectedOptions[userId].includes(selectedOption)) {
+        // Deselect the option
+        selectedOptions[userId] = selectedOptions[userId].filter(
+            option => option !== selectedOption
+        );
+    } else {
+        // Select the option
+        selectedOptions[userId].push(selectedOption);
+    }
+
+    const selectedText =
+        selectedOptions[userId].length === 0
+            ? "No topics selected"
+            : `Selected topics: ${selectedOptions[userId].join(", ")}`;
+
+    return ctx.editMessageText(selectedText);
+});
+
+// AWS event handler syntax
 exports.handler = async event => {
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: "" };
-  } catch (e) {
-    console.error("error in handler:", e);
-    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" };
-  }
-}
+    try {
+        await bot.handleUpdate(JSON.parse(event.body));
+        return { statusCode: 200, body: "" };
+    } catch (e) {
+        console.error("error in handler:", e);
+        return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" };
+    }
+};
