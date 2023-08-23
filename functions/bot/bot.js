e0fd78f3e5c38f35765b0d9a856cc21ce7585245
mabhishek7081 <mabhishek7081@gmail.com>
2023-08-23 16:28:10 +0530

    improved api

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -113,7 +113,7 @@ bot.hears(/Array|String|Linked List|Doubly-Linked List|Stack|Queue|Tree|Graph|Gr
     const username = ctx.message.from.username;
     // console.log(ctx.message);
     try {
-        fetch("https://script.google.com/macros/s/AKfycbw584DxzF9SghwdqrlI8rF6MfGZnwY2wLWGJp1p_BZAp7fhLh0QwvRBbBmawSZV06-O/exec"+ "?action=addUser&username=" + username + "&topic=" + topic, {
+        fetch("https://script.google.com/macros/s/AKfycbw584DxzF9SghwdqrlI8rF6MfGZnwY2wLWGJp1p_BZAp7fhLh0QwvRBbBmawSZV06-O/exec"+ "?username=" + username + "&topic=" + topic, {
             method: 'POST'
         })
     } catch (e) {   
